7153e4407a37c259b4221749c2a1c53d08244fa9
vishnu-nara <147574515+vishnu-nara@users.noreply.github.com>
2023-10-11 09:50:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const headerTitle = document.querySelector('.header-title');
+
+        // Add a click event listener to the header title
+        headerTitle.addEventListener('click', function() {
+            window.location.href = 'index.html'; // Replace 'index.html' with the actual URL of your home page
+        });
